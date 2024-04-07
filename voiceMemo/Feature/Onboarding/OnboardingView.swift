0cca5a0d6d37a37c5331a65072e4e16984ac3b4d
DasanKim <tempzipp@gmail.com>
2024-04-08 00:32:04 +0900

    ✨Feat: HomeView 구현 및 OnboadingView와 연결

--- voiceMemo/Feature/Onboarding/OnboardingView.swift
+++ voiceMemo/Feature/Onboarding/OnboardingView.swift
@@ -13,11 +13,7 @@ struct OnboardingView: View {
     
     var body: some View {
         NavigationStack(path: $pathModel.paths) {
-            //OnboardingContentView(onboardingViewModel: onboardingViewModel)
-            //TodoListView()
-            //MemoListView()
-            TimerView()
-                .environmentObject(memoListViewModel)
+            OnboardingContentView(onboardingViewModel: onboardingViewModel)
                 .navigationDestination(
                     for: PathType.self,
                     destination: { pathType in
@@ -25,6 +21,8 @@ struct OnboardingView: View {
                         case .homeView:
                             HomeView()
                                 .navigationBarBackButtonHidden()
+                                .environmentObject(todoListViewModel)
+                                .environmentObject(memoListViewModel)
                             
                         case .todoView:
                             TodoView()
