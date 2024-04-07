21767242ee66858bc3f37e9cff3a799a7b25a603
DasanKim <tempzipp@gmail.com>
2024-04-08 00:50:48 +0900

    ♻️Refactor: 필요한 뷰에 homeViewModel과 관련된 설정 추가하여 바인딩

--- voiceMemo/Feature/Home/HomeView.swift
+++ voiceMemo/Feature/Home/HomeView.swift
@@ -63,6 +63,7 @@ struct HomeView: View {
                     .tag(Tab.setting)
                 
             }
+            .environmentObject(homeViewModel)
             
             SeperatorLineView()
         }
