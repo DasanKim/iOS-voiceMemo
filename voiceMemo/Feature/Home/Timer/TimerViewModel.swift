23bd4446e9952ac89c57e8fecec21bdda6ee067a
DasanKim <tempzipp@gmail.com>
2024-04-07 23:24:09 +0900

    ✨Feat: Timer에 User Notification 적용

--- voiceMemo/Feature/Home/Timer/TimerViewModel.swift
+++ voiceMemo/Feature/Home/Timer/TimerViewModel.swift
@@ -4,6 +4,7 @@
 //
 
 import Foundation
+import UIKit
 
 class TimerViewModel: ObservableObject {
     @Published var isDisplaySetTimeView: Bool
@@ -11,19 +12,22 @@ class TimerViewModel: ObservableObject {
     @Published var timer: Timer?
     @Published var timeRemaining: Int
     @Published var isPaused: Bool
+    var notificationService: NotificationService
     
     init(
         isDisplaySetTimeView: Bool = true,
         time: Time = .init(hours: 0, minutes: 0, seconds: 0),
         timer: Timer? = nil,
         timeRemaining: Int = 0,
-        isPaused: Bool = false
+        isPaused: Bool = false,
+        notificationService: NotificationService = .init()
     ) {
         self.isDisplaySetTimeView = isDisplaySetTimeView
         self.time = time
         self.timer = timer
         self.timeRemaining = timeRemaining
         self.isPaused = isPaused
+        self.notificationService = notificationService
     }
 }
 
@@ -55,6 +59,16 @@ private extension TimerViewModel {
     func startTimer() {
         guard timer == nil else { return }
         
+        // 백그라운드에서도 동작할 수 있도록
+        var backgroundTaskID: UIBackgroundTaskIdentifier?
+        backgroundTaskID = UIApplication.shared.beginBackgroundTask { // 앱이 백그라운드로 전환되었을 때 일부 작업을 계속 수행할 수 있게함. 이게 있어야 백그라운드에서도 타이머가 흐를 수 있음
+            // 백그라운드 실행시간이 소진되어갈때 이 핸들러를 호출함. 위에서 반환된 backgroundTaskID으로 작업을 종료시킴
+            if let task = backgroundTaskID {
+                UIApplication.shared.endBackgroundTask(task) //
+                backgroundTaskID = .invalid
+            }
+        }
+        
         timer = Timer.scheduledTimer(
             withTimeInterval: 1, repeats: true
         ) { _ in
@@ -62,6 +76,12 @@ private extension TimerViewModel {
                 self.timeRemaining -= 1
             } else {
                 self.stopTimer()
+                self.notificationService.sendNotification()
+                
+                if let task = backgroundTaskID {
+                    UIApplication.shared.endBackgroundTask(task)
+                    backgroundTaskID = .invalid
+                }
             }
         }
     }
